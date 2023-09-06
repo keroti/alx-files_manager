abab6346f819a650af23256656b3d158a55a2bdd
keroti <peterkeroti55@gmail.com>
2023-09-06 19:38:37 +0300

    First API

--- controllers/AppController.js
+++ controllers/AppController.js
@@ -1,20 +1,20 @@
-import dbClient from "../utils/db";
-import redisClient from "../utils/redis";
+import dbClient from '../utils/db';
+import redisClient from '../utils/redis';
 
 class AppController {
-    static async getStatus(req, res) {
-        const redisStatus = redisClient.isAlive();
-        const dbStatus = dbClient.isAlive();
+  static async getStatus(req, res) {
+    const redisStatus = redisClient.isAlive();
+    const dbStatus = dbClient.isAlive();
 
-        res.status(200).json({ redis: redisStatus, db: dbStatus });
-    }
+    res.status(200).json({ redis: redisStatus, db: dbStatus });
+  }
 
-    static async getStats(req, res) {
-        const usersCount = await dbClient.nbUsers();
-        const filesCount = await dbClient.nbFiles();
+  static async getStats(req, res) {
+    const usersCount = await dbClient.nbUsers();
+    const filesCount = await dbClient.nbFiles();
 
-        res.status(200).json({ users: usersCount, files: filesCount });
-    }
+    res.status(200).json({ users: usersCount, files: filesCount });
+  }
 }
 
 export default AppController;

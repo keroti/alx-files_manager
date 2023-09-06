fba4e242a0246739148302b32a05f7e1f84e18a9
keroti <peterkeroti55@gmail.com>
2023-09-06 19:15:58 +0300

    Redis Utils

--- utils/redis.js
+++ utils/redis.js
@@ -1,41 +1,41 @@
-import { createClient } from "redis";
-import { promisify } from "util";
+import { createClient } from 'redis';
+import { promisify } from 'util';
 
 class RedisClient {
-    constructor() {
-        this.myClient = createClient();
-        this.myClient.on("error", (error) => console.log(error));
-    }
+  constructor() {
+    this.myClient = createClient();
+    this.myClient.on('error', (error) => console.log(error));
+  }
 
-    isAlive() {
-        return this.myClient.connected;
-    }
+  isAlive() {
+    return this.myClient.connected;
+  }
 
-    async get(key) {
-        const getAsync = promisify(this.myClient.get).bind(this.myClient);
-        return getAsync(key);
-    }
+  async get(key) {
+    const getAsync = promisify(this.myClient.get).bind(this.myClient);
+    return getAsync(key);
+  }
 
-    async set(key, val, time) {
-        const setAsync = promisify(this.myClient.set).bind(this.myClient);
-        return setAsync(key, val, "EX", time);
-    }
+  async set(key, val, time) {
+    const setAsync = promisify(this.myClient.set).bind(this.myClient);
+    return setAsync(key, val, 'EX', time);
+  }
 
-    async del(key) {
-        const delAsync = promisify(this.myClient.del).bind(this.myClient);
-        return delAsync(key);
-    }
+  async del(key) {
+    const delAsync = promisify(this.myClient.del).bind(this.myClient);
+    return delAsync(key);
+  }
 
-    close() {
-        this.myClient.quit();
-    }
+  close() {
+    this.myClient.quit();
+  }
 }
 
 const redisClient = new RedisClient();
 
 // Close the Redis client when your application exits
-process.on("exit", () => {
-    redisClient.close();
+process.on('exit', () => {
+  redisClient.close();
 });
 
 export default redisClient;

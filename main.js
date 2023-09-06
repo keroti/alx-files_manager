cd841985aa77f26dba0df94c4867c904b8a935e0
keroti <peterkeroti55@gmail.com>
2023-09-06 19:34:46 +0300

    First API

--- main.js
+++ main.js
@@ -1,12 +1,44 @@
-import redisClient from './utils/redis';
+import { MongoClient } from "mongodb";
 
-(async () => {
-    console.log(redisClient.isAlive());
-    console.log(await redisClient.get('myKey'));
-    await redisClient.set('myKey', 12, 5);
-    console.log(await redisClient.get('myKey'));
+class DBClient {
+    constructor() {
+        this.host = process.env.DB_HOST || "localhost";
+        this.port = process.env.DB_PORT || 27017;
+        this.database = process.env.DB_DATABASE || "files_manager";
+        this.client = new MongoClient(
+            `mongodb://${this.host}:${this.port}`
+        );
+        this.client
+            .connect()
+            .then(() => {
+                console.log("DBClient connected to MongoDB");
+            })
+            .catch((err) => {
+                console.error("Error connecting to MongoDB:", err);
+            });
+    }
 
-    setTimeout(async () => {
-        console.log(await redisClient.get('myKey'));
-    }, 1000*10)
-})();
+    isAlive() {
+        return this.client.isConnected();
+    }
+
+    async nbUsers() {
+        const usersCollection = this.client
+            .db(this.database)
+            .collection("users");
+        const userCount = await usersCollection.countDocuments();
+        return userCount;
+    }
+
+    async nbFiles() {
+        const filesCollection = this.client
+            .db(this.database)
+            .collection("files");
+        const fileCount = await filesCollection.countDocuments();
+        return fileCount;
+    }
+}
+
+const dbClient = new DBClient();
+
+export default dbClient;

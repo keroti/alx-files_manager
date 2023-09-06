1d797e0d40458b2e53792a1631cf2dc36e690adb
keroti <peterkeroti55@gmail.com>
2023-09-06 19:44:58 +0300

    Create new user

---
+++ controllers/UsersController.js
@@ -0,0 +1,35 @@
+import { hash } from 'bcrypt';
+import dbClient from '../utils/db';
+
+class UsersController {
+  static async postNew(req, res) {
+    const { email, password } = req.body;
+
+    if (!email) {
+      return res.status(400).json({ error: 'Missing email' });
+    }
+
+    if (!password) {
+      return res.status(400).json({ error: 'Missing password' });
+    }
+
+    const existingUser = await dbClient.users.findOne({ email });
+
+    if (existingUser) {
+      return res.status(400).json({ error: 'Already exist' });
+    }
+
+    const hashedPassword = await hash(password, 10);
+
+    const newUser = {
+      email,
+      password: hashedPassword,
+    };
+
+    const result = await dbClient.users.insertOne(newUser);
+
+    return res.status(201).json({ id: result.insertedId, email });
+  }
+}
+
+export default UsersController;
